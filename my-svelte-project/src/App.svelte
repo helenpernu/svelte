853b4d74db321c338853756b22ec201320b0712a
Helen <helen.pernu@skola.botkyrka.se>
2021-05-31 21:20:15 +0200

    todo list function

---
+++ my-svelte-project/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+    let newItem = "";
+    
+    let todoList = [{text: "Städa rummet", status: true},
+                    {text: "Plugga kemi", status: false},
+                    {text: "Läsa svenska boken", status: false}];
+    
+    function addToList() {
+        todoList = [...todoList, {text: newItem, status: false}];
+        newItem = "";
+    }
+</script>
